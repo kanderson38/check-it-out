9777d4aecca50fd61131ebcd57e1035494245227
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-07-09 11:47:17 -0700

    styling

---
+++ build/precache-manifest.5673e2c9e82c8ad0a2ffc7a48fd1ab70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c381c65cba094282192651396050fac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "790051c1db180139f180",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cffe4e93d69ee568fa06",
+    "url": "/static/js/2.2fa0f04c.chunk.js"
+  },
+  {
+    "revision": "790051c1db180139f180",
+    "url": "/static/js/main.4907433a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css'
 import Home from "./components/Home.js";
+import Books from './components/Books.js';
+
 import withFirebaseAuth from 'react-with-firebase-auth'
 import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
@@ -13,11 +15,6 @@ const providers = {
   googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
-
-
-function About() {
-  return <h2>About</h2>;
-}
 
 function Users() {
   return <h2>Users</h2>;
@@ -35,33 +32,42 @@ class App extends Component {
       <Router>
         <div>
           <nav>
+            <div className="login">
+              {
+                user
+                  ? <span className="login-span">Hello, {user.displayName}</span>
+                  : <span className="login-span">Please sign in.</span>
+              }
+              {
+                user
+                  ? <span className="login-button" onClick={signOut}>Sign out</span>
+                  : <span className="login-button" onClick={signInWithGoogle}>Sign in with Google</span>
+              }
+            </div>
+
+            <h1 class="logo">Check It Out</h1>
+
             <ul>
               <li>
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/about/">About</Link>
+                <Link to="/books/">Full Library</Link>
               </li>
               <li>
                 <Link to="/users/">Users</Link>
               </li>
             </ul>
           </nav>
+          <div className="status">
+            
+          </div>
 
           <Route path="/" exact component={Home} />
-          <Route path="/about/" component={About} />
+          <Route path="/books/" component={Books} />
           <Route path="/users/" component={Users} />
 
-          {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          }
+
         </div>
       </Router>
     )
