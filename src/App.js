1a7d7dfd9fee947170b595437e39d65918bb79a6
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-07-09 10:47:37 -0700

    trying to ignore config info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css'
 import Home from "./components/Home.js";
+import withFirebaseAuth from 'react-with-firebase-auth'
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+import * as firebase from 'firebase/app';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
 
 
 
@@ -13,31 +23,53 @@ function Users() {
   return <h2>Users</h2>;
 }
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
-        </nav>
+class App extends Component {
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
 
-        <Route path="/" exact component={Home} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
-      </div>
-    </Router>
-  );
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+        </div>
+      </Router>
+    )
+  };
 }
 
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
